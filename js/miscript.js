88f3cea5fdc902d2901024986f221211fa639c25
Michel <tester.qa.michel@gmail.com>
2024-02-14 20:11:40 -0300

    Mi primer commit

---
+++ js/miscript.js
@@ -0,0 +1,20 @@
+function funcion1()
+{
+let email = document
+
+}
+
+/*for (i = 1; i = 5; i++){
+    console.log(i)
+}*/
+
+function factorial() {
+    let factorial = document.getElementById('numFactorial').value
+    console.log(factorial);
+    let fact = 1;
+    for(i=1;i <= factorial; i++) {
+        fact = fact*i;
+}
+console.log("el factorial de " + factorial + " es: " + fact);
+
+}
